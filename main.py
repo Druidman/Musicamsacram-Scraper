545d9aaf32520218a31e35c4de4c4a2b3d107d73
Mateusz Kowalski <towermanxxl@gmail.com>
2024-12-17 21:47:53 +0100

    multithreaded version

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from scraper import *
 from document import getDocument
 import json
+from threading import Thread
 
 def appendVersesToSong(song: dict, verses: list):
     for verse in verses:
@@ -18,8 +19,69 @@ def filterLyrics(song: dict):
 
     song["lyrics"] = lyrics
 
-
+def handleSong(songObj,songs):
+    title = songObj["title"]
+    songLink = songObj["link"]
+    print(f"           with link: {songLink}")
     
+
+    songDocument = getDocument(link=songLink)
+    verses = getVerses(songDocument=songDocument)
+    song = {
+        "title": title,
+        "lyrics": []
+        }
+    
+    appendVersesToSong(song,verses)
+
+    filterLyrics(song)
+    
+    songs.append(song)
+
+def handlePage(pageLink,songs):
+    
+    pageDocument = getDocument(link=pageLink)
+    songObjects = getSongObjects(pageDocument=pageDocument)
+    
+    threads = []
+    for songObj in songObjects:
+        thread = Thread(target=handleSong,args=(songObj,songs))
+        threads.append(thread)
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+def handleCategory(categoryObj,data):
+    category = categoryObj["category"]
+    categoryLink = categoryObj["link"]
+    print(f"starting work on: {category} category")
+    print(f"link {categoryLink}")
+
+    categoryDocument = getDocument(link=categoryLink)
+    pageLinks = getPageLinks(categoryDocument=categoryDocument,firstLink=categoryLink)
+    print("got page links")
+    songs = []
+
+
+    threads = []
+    for pageLink in pageLinks:
+        thread = Thread(target=handlePage,args=(pageLink,songs))
+        threads.append(thread)
+
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+        
+            
+        
+        
+    data["data"]["categories"][category] = songs
+
     
 
 mainLink = "https://musicamsacram.pl/spiewnik/kategorie"
@@ -35,46 +97,17 @@ data = {
 print("Scraper start...\n\n")
 categoryObjects = getCategoryObjects(mainDocument=mainDocument)
 print("Got categories\n")
+
+threads = []
 for categoryObj in categoryObjects:
+    thread = Thread(target=handleCategory,args=(categoryObj,data))
+    threads.append(thread)
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
     
-    
-    category = categoryObj["category"]
-    categoryLink = categoryObj["link"]
-    print(f"starting work on: {category} category")
-    print(f"link {categoryLink}")
-
-    categoryDocument = getDocument(link=categoryLink)
-    pageLinks = getPageLinks(categoryDocument=categoryDocument,firstLink=categoryLink)
-    print("got page links")
-    songs = []
-    i = 1
-    for pageLink in pageLinks:
-        pageDocument = getDocument(link=pageLink)
-        songObjects = getSongObjects(pageDocument=pageDocument)
-        
-        
-        for songObj in songObjects:
-            title = songObj["title"]
-            songLink = songObj["link"]
-            print(f"           {i} with link: {songLink}")
-            i+=1
-
-            songDocument = getDocument(link=songLink)
-            verses = getVerses(songDocument=songDocument)
-            song = {
-                "title": title,
-                "lyrics": []
-                }
-            
-            appendVersesToSong(song,verses)
-
-            filterLyrics(song)
-            
-            songs.append(song)
-            
-        
-        
-    data["data"]["categories"][category] = songs
     print("\n\nassigned songs to category\n\n")
     break
         
